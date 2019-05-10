f642aeef7a3918e0b3ffa9d539851ee11a75d3e0
Clay <rholt16@uncc.edu>
2019-05-10 10:16:34 -0400

    frontend update

---
+++ public/precache-manifest.92dcf306fad68e7fb1e6820cb68c11f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "97ae4d4cfa6f63816c8f8b41441f266e",
+    "url": "/static/media/myMapMarker.97ae4d4c.png"
+  },
+  {
+    "revision": "6c45d3a757128816f7af0b12345138cd",
+    "url": "/static/media/myLogo.6c45d3a7.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45dba0a15ba9dd1cc182",
+    "url": "/static/js/main.5a2aa5b6.chunk.js"
+  },
+  {
+    "revision": "51ca86f306e8045d5a31",
+    "url": "/static/js/2.fe9ac901.chunk.js"
+  },
+  {
+    "revision": "45dba0a15ba9dd1cc182",
+    "url": "/static/css/main.8bc2fce3.chunk.css"
+  },
+  {
+    "revision": "7c31ae7bf932403ffea2a00aefa5ccc4",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b0e020fa5fc58893856922dc9b6e2898.js"
+  "/precache-manifest.92dcf306fad68e7fb1e6820cb68c11f0.js"
 );
 
 workbox.clientsClaim();
